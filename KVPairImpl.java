c5b84fbc11aad29d3e988341716de2e450980bae
Devashish Priyadarshi <devashish234073@gmail.com>
2018-05-20 07:55:50 +0530

    Update KVPairImpl.java

--- KVPairImpl.java
+++ KVPairImpl.java
@@ -2,23 +2,23 @@ public class KVPairImpl {
     public static void main(String[] args)  {
         KVPair kv = new KVPair(10);
         kv.put("1006010","devashish");
-        kv.put("1006011","pranav");
-        kv.put("1006012","amit");
-        kv.put("1006013","amrish");
-        kv.put("1006014","rajeev");
-        kv.put("1006015","suresh");
-        kv.put("1006016","test1");
-        kv.put("1006017","test2");
-        kv.put("1006018","test3");
-        kv.put("1006019","test4");
+        kv.put("1006011","abc");
+        kv.put("1006012","DEF");
+        kv.put("1006013","gHIj");
+        kv.put("1006014","kLmNo");
+        kv.put("1006015","#$*&");
+        kv.put("1006016","-----");
+        kv.put("1006017","._._._._.");
+        kv.put("1006018","////////");
+        kv.put("1006019","NOOP");
         kv.put("1006020","test5");
-        kv.put("1006021","test6");
+        kv.put("1006021","GOOF");
         kv.put("1006022","test7");
-        kv.put("1006023","test8");
+        kv.put("1006023","SOUP");
         kv.put("1006024","test9");
-        kv.put("1006025","test91");
+        kv.put("1006025","BLOOP");
         kv.put("1006026","test92");
-        kv.put("1006027","test93");
+        kv.put("1006027","DUPE");
 
         kv.dispHashTableKV();
     }
